082fa99717c21d5716c83170731aa27df645a33c
SpoonTooBig <collinfaeth16@gmail.com>
2026-02-16 16:16:00 -0500

    Comment out prismatic joint for the time being

--- DrawBot.m
+++ DrawBot.m
@@ -6,14 +6,16 @@ deg = pi/180;
 L(1) = Revolute('d', 0, ...   % link length (Dennavit-Hartenberg notation)
     'a', 8.8, ...               % link offset (Dennavit-Hartenberg notation)
     'alpha', 0, ...        % link twist (Dennavit-Hartenberg notation)
+    'offset', .0038*deg, ...
     'qlim', [-160 160]*deg ); % minimum and maximum joint angle
 
 L(2) = Revolute('d', 1.8, ...   % link length (Dennavit-Hartenberg notation)
     'a', 10.5, ...               % link offset (Dennavit-Hartenberg notation)
     'alpha', 0, ...        % link twist (Dennavit-Hartenberg notation)
+    'offset', .0038*deg, ...
     'qlim', [-160 160]*deg ); % minimum and maximum joint angle
 
-L3 = Prismatic('theta', pi/2, 'a', 0, 'alpha', 0, 'qlim', [0 12.5]);
+% L(3) = Prismatic('theta', pi/2, 'a', 0, 'alpha', 0);
 
 qz = [0 0 0]; % zero angles, L shaped pose
 
