e185645767b34ae188063dc636d2a40851189822
SpoonTooBig <collinfaeth16@gmail.com>
2026-02-24 13:49:02 -0500

    Added minimization function starter

---
+++ Minimization.m
@@ -0,0 +1,15 @@
+robot = DrawBot;
+
+ds = robot.d';
+as = robot.a';
+alphas = robot.alpha';
+offsets = robot.offset';
+
+dhParams = [ds, as, alphas, offsets];
+base = [12.2; 1.8];
+
+robotParams = [dhParams base];
+options = optimset('MaxFunEvals', 10000)
+fminsearch(@(robotParams)DHObjFunc(robotParams), robotParams, options)
+
+robotParams
