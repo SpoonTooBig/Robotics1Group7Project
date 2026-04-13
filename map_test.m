5c00060ebe9e01f8269bf42605910760206f194e
jfish510 <jeni.fischer10@gmail.com>
2026-04-13 14:57:27 -0400

    map smiley with goal

--- map_test.m
+++ map_test.m
@@ -1,14 +1,13 @@
-
 map = zeros(100,100);
 
-map(10:16,8:40) = 1;%left eyebrow
-map(10:16,60:92) = 1;%right eyebrow
-map(22:34,22:36) = 1;%left eye
-map(22:34,64:78) = 1;%right eye
+map(84:90,8:40) = 1;%left eyebrow
+map(84:90,60:92) = 1;%right eyebrow
+map(66:78,22:36) = 1;%left eye
+map(66:78,64:78) = 1;%right eye
 map(40:46,40:60) = 1;%nose
-map(50:76,10:16) = 1;%left smile 
-map(50:76,84:90) = 1;%right smile 
-map(76:88,10:90) = 1;%bottom smile 
+map(24:50,10:16) = 1;%left smile 
+map(24:50,84:90) = 1;%right smile 
+map(10:24,10:90) = 1;%bottom smile 
 
 figure(1)
 imagesc(map)
@@ -19,3 +18,12 @@ axis([0 100 0 100]);
 title('100x100 Costmap');
 xlabel('X-axis');
 ylabel('Y-axis');
+
+
+goal = [50,50];
+start = [1,2];
+dx = DXform(map);
+dx.plan(goal);
+p = dx.query(start,'animate');
+%p = dx.path(start);
+numrows(p)
